f6e67430d70181a82989b0069ce732375b744b1e
Miles Saul <mileshsaul@yahoo.com>
2015-10-17 22:29:56 -0700

    Added feature to make map center on current location

--- static/locations.js
+++ static/locations.js
@@ -1,10 +1,17 @@
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
+var x = document.getElementById("demo");
+  
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap);
+        alert(position.coords.latitude;
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    position.coords.latitude;
+    position.coords.longitude;  
 }
 
 function newLocation() {
